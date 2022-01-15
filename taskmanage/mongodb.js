f672b711b2a19704fdad0280ebf8e5c6a89ef634
KumariDeeksha <deeku2117@gmail.com>
2022-01-15 11:56:46 +0530

    insertion documents

--- taskmanage/mongodb.js
+++ taskmanage/mongodb.js
@@ -1,13 +1,62 @@
 //CRUD operations
 
 const mongodb= require('mongodb')
-const MongoClient = mongodb.Mongoclient
+const MongoClient = mongodb.MongoClient
 
 const connectionURL='mongodb://127.0.0.1:27017'
 const databaseName='taskmanage'
 
-MongoClient.connect(connectionURL,{useNewParser:true},(error,client)=>{
+MongoClient.connect(connectionURL,{useNewUrlParser:true},(error,client)=>{
  if(error){
      return console.log("Unable to connect to database!")
  }
+  const db =client.db(databaseName)
+  db.collection('users').insertOne({
+      name: 'Deeksha',
+      age: 21
+  },(error,result) =>{
+    if(error){
+    return console.log('unable to insert user')}
+    console.log(result.ops)
+    // ops is an array of documents
+  })
+    db.collection('users').insertMany([
+      {
+          name:'jen',
+          age:34
+    },
+    {
+      name:'harry',
+      age:32
+    }, (error,result)=>{
+       if(error){
+        return console.log('unable to insert user')}
+        
+       
+       console.log(result.ops)
+    }
+  ])
+
+
+// #challenge
+ db.collection('tasks').insertMany([
+   {
+     description:'clean the house',
+     completed: true
+   },
+   {
+    description:'renew inspection',
+    completed: false
+   },
+   {
+    description:'take care of pots',
+    completed: true
+   } 
+  ],(error,result)=>{
+        if(error){
+        return console.log('unable to insert document!')
+   }
+   console.log(result.ops)
+  })
 })
+
